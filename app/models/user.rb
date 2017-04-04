28fe12cc2f85b80fd1fbed70cb8631fa4dc5a7b1
boxuan lu <boxuan@ualberta.ca>
2017-04-04 02:22:02 +0000

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User <ActiveRecord::Base
+    has_many:posts
+    has_many:comments
+    has_many:likes
+    
 end
 user= User.new({username:"shark_j",avatar_url: "http://naserca.com/images/shark_j.jpg"})
